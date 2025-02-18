bd58e64491fe524e565d8e8977e6a14af508bf83
goni715 <halesgoni715149123@gmail.com>
2025-02-18 18:11:16 +0600

    tailwind-css setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 const App = () => {
   return (
     <>
-      <h1>This is Todo Front with mysql project</h1>
+      <h1 className="text-gray-400 font-bold bg-amber-800">This is Todo Front with mysql project</h1>
     </>
   );
 };
