d516857ba2055373a4003692da770fce7df43676
goni715 <halesgoni715149123@gmail.com>
2025-02-18 18:28:30 +0600

    daisyui setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ const App = () => {
   return (
     <>
       <h1 className="text-gray-400 font-bold bg-amber-800">This is Todo Front with mysql project</h1>
+      <button className="btn">Button</button>
+      <button className="btn btn-secondary">Secondary</button>
     </>
   );
 };
