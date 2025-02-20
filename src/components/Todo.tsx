9d107b8002b162b180a86205694bb0b3099a57de
goni715 <halesgoni715149123@gmail.com>
2025-02-20 11:12:23 +0600

    type initiate

---
+++ src/components/Todo.tsx
@@ -0,0 +1,38 @@
+import { FaEdit, FaRegTrashAlt } from "react-icons/fa";
+import { ITodo } from "../types/todo.type";
+
+type TProps = {
+    item: ITodo,
+    index: number;
+}
+
+const Todo = ({item, index}: TProps) => {
+   
+    
+    return (
+        <>
+           <tr>
+                <td className="px-4 py-2 border border-gray-300 whitespace-nowrap">
+                 {Number(index+1)}
+                </td>
+                <td className="px-4 py-2 border border-gray-300 whitespace-nowrap">
+                 {item?.name}
+                </td>
+                <td className="px-4 py-2 border border-gray-300 whitespace-nowrap">
+                  {item?.email}
+                </td>
+                <td className="px-4 py-2 border border-gray-300 whitespace-nowrap flex items-center justify-center gap-3">
+                  <button className="bg-green-500 p-2 rounded-full text-white text-xl cursor-pointer">
+                    <FaEdit />
+                  </button>
+                  <button className="bg-red-500 p-2 rounded-full text-white text-xl cursor-pointer">
+                    <FaRegTrashAlt />
+                  </button>
+                </td>
+              </tr>
+              
+        </>
+    );
+};
+
+export default Todo;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,76 +1,59 @@
-import { FaEdit, FaRegTrashAlt } from "react-icons/fa";
+import Todo from "../components/Todo";
 import { useGetAllTodosQuery } from "../redux/features/api/baseApi";
+import { ITodo } from "../types/todo.type";
 
 const HomePage = () => {
-    const { data, isLoading } = useGetAllTodosQuery(undefined);
-    console.log(data);
+  const { data, isLoading } = useGetAllTodosQuery(undefined);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (data?.length === 0) {
     return (
-        <>
-            <h1>This is Home Page</h1>
-            <div className="p-4">
-        <div className="overflow-x-auto">
-          <table className="min-w-full bg-white border border-gray-300">
-            <thead>
-              <tr>
-                <th className="px-4 py-2 border border-gray-300 whitespace-nowrap">
-                  Serial
-                </th>
-                <th className="px-4 py-2 border border-gray-300 whitespace-nowrap">
-                  Name
-                </th>
-                <th className="px-4 py-2 border border-gray-300 whitespace-nowrap">
-                  Email
-                </th>
-                <th className="px-4 py-2 border border-gray-300 whitespace-nowrap">
-                  Action
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td className="px-4 py-2 border border-gray-300 whitespace-nowrap">
-                  1
-                </td>
-                <td className="px-4 py-2 border border-gray-300 whitespace-nowrap">
-                  Osman Goni
-                </td>
-                <td className="px-4 py-2 border border-gray-300 whitespace-nowrap">
-                  goni@gmail.com
-                </td>
-                <td className="px-4 py-2 border border-gray-300 whitespace-nowrap flex items-center justify-center gap-3">
-                  <button className="bg-green-500 p-2 rounded-full text-white text-xl cursor-pointer">
-                    <FaEdit />
-                  </button>
-                  <button className="bg-red-500 p-2 rounded-full text-white text-xl cursor-pointer">
-                    <FaRegTrashAlt />
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td className="px-4 py-2 border border-gray-300 whitespace-nowrap">
-                  2
-                </td>
-                <td className="px-4 py-2 border border-gray-300 whitespace-nowrap">
-                  Evan Ahmed
-                </td>
-                <td className="px-4 py-2 border border-gray-300 whitespace-nowrap">
-                  evan@gmail.com
-                </td>
-                <td className="px-4 py-2 border border-gray-300 whitespace-nowrap flex items-center justify-center gap-3">
-                  <button className="bg-green-500 p-2 rounded-full text-white text-xl cursor-pointer">
-                    <FaEdit />
-                  </button>
-                  <button className="bg-red-500 p-2 rounded-full text-white text-xl cursor-pointer">
-                    <FaRegTrashAlt />
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+      <div className="text-center text-3xl text-gray-400 mt-48">
+        There is no todo
       </div>
-        </>
     );
+  }
+
+  if (data?.length > 0) {
+    return (
+      <>
+        <div className="p-4">
+          <div className="overflow-x-auto">
+            <table className="min-w-full bg-white border border-gray-300">
+              <thead>
+                <tr>
+                  <th className="px-4 py-2 border border-gray-300 whitespace-nowrap">
+                    Serial
+                  </th>
+                  <th className="px-4 py-2 border border-gray-300 whitespace-nowrap">
+                    Name
+                  </th>
+                  <th className="px-4 py-2 border border-gray-300 whitespace-nowrap">
+                    Email
+                  </th>
+                  <th className="px-4 py-2 border border-gray-300 whitespace-nowrap">
+                    Action
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  data?.map((item:ITodo,i:number)=>(
+                    <Todo key={i} item={item} index={i}/>
+                  ))
+                }
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  return <></>;
 };
 
 export default HomePage;
