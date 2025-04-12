b4e4084995af0e638c158a0e7283545edeb3ae77
goni715 <halesgoni715149123@gmail.com>
2025-04-12 11:47:00 +0600

    create TodoTable

---
+++ src/components/TodoTable.tsx
@@ -0,0 +1,39 @@
+
+import { ITodo } from './../types/todo.type';
+const TodoTable = ({data} : {data: ITodo[]}) => {
+    return (
+        <>
+            <div className="p-4">
+          <div className="overflow-x-auto">
+            <table className="min-w-full bg-white border border-gray-300">
+              <thead>
+                <tr>
+                  <th className="px-4 py-2 border border-gray-300 whitespace-nowrap">
+                    Serial
+                  </th>
+                  <th className="px-4 py-2 border border-gray-300 whitespace-nowrap">
+                    Name
+                  </th>
+                  <th className="px-4 py-2 border border-gray-300 whitespace-nowrap">
+                    Email
+                  </th>
+                  <th className="px-4 py-2 border border-gray-300 whitespace-nowrap">
+                    Action
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  data?.map((item:ITodo,i:number)=>(
+                    <Todo key={i} item={item} index={i}/>
+                  ))
+                }
+              </tbody>
+            </table>
+          </div>
+        </div>
+        </>
+    );
+};
+
+export default TodoTable;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -20,40 +20,11 @@ const HomePage = () => {
   if (data?.length > 0) {
     return (
       <>
-        <div className="p-4">
-          <div className="overflow-x-auto">
-            <table className="min-w-full bg-white border border-gray-300">
-              <thead>
-                <tr>
-                  <th className="px-4 py-2 border border-gray-300 whitespace-nowrap">
-                    Serial
-                  </th>
-                  <th className="px-4 py-2 border border-gray-300 whitespace-nowrap">
-                    Name
-                  </th>
-                  <th className="px-4 py-2 border border-gray-300 whitespace-nowrap">
-                    Email
-                  </th>
-                  <th className="px-4 py-2 border border-gray-300 whitespace-nowrap">
-                    Action
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {
-                  data?.map((item:ITodo,i:number)=>(
-                    <Todo key={i} item={item} index={i}/>
-                  ))
-                }
-              </tbody>
-            </table>
-          </div>
-        </div>
+        
       </>
     );
   }
 
-  return <></>;
 };
 
 export default HomePage;
