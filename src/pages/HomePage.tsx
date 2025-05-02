395a111f795df04612b72cc42abb38e27e160694
goni715 <halesgoni715149123@gmail.com>
2025-05-02 16:36:46 +0600

    design correction

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -9,7 +9,7 @@ const HomePage = () => {
           <div className="py-4 px-8 fixed flex justify-end w-full top-0 right-0 bg-gray-900">
             <CreateTodoModal />
           </div>
-          <div className="px-6 mt-[80px] py-2">
+          <div className="px-0 sm:px-6 mt-[80px] py-2">
           <TodoList />
           </div>
         </div>
